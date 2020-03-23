23a10a280e3f32be9685b6afbbfa2f4527010de8
VebionPlays <49697812+VebionPlays@users.noreply.github.com>
2020-03-23 15:32:58 +0100

    Add files via upload

---
+++ BlockBase.java
@@ -0,0 +1,35 @@
+package vebion.potterblocks.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import vebion.potterblocks.Main;
+import vebion.potterblocks.init.ModBlocks;
+import vebion.potterblocks.init.ModItems;
+import vebion.potterblocks.util.IHasModel;
+
+public class BlockBase extends Block implements IHasModel
+{
+
+	public BlockBase(String name, Material material)
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		
+		ModBlocks.BLOCKS.add(this);
+		ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+	}
+
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+	}
+	
+	
+	
+}
